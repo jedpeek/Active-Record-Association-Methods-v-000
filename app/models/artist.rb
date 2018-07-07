c06d3130139ba7e358467f75723a56cbca0062c3
Jed Peek <jedpeek@yahoo.com>
2018-07-07 16:03:57 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -13,7 +13,7 @@ class Artist < ActiveRecord::Base
   end
 
   def genre_count
-    self.songs.genre.uniq
+    self.songs.genres.uniq
     #return the number of genres associated with the artist
   end
 end
